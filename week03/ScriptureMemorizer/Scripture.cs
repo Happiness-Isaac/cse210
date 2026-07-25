682f57a0f4e1ffe52d7dded0c4408dcb40f55dd3
Happiness-Isaac <mmabahappiness@gmail.com>
2026-07-25 21:31:35 +0100

    Week 03

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,71 @@
+public class Scripture
+{
+
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random;
+
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        _random = new Random();
+        string[] parts = text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string part in parts)
+        {
+            _words.Add(new Word(part));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        List<Word> visibleWords = new List<Word>();
+
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                visibleWords.Add(word);
+            }
+        }
+
+        int wordsToHide = Math.Min(numberToHide, visibleWords.Count);
+
+        for (int i = 0; i < wordsToHide; i++)
+        {
+            int index = _random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string scriptureText = "";
+
+        for (int i = 0; i < _words.Count; i++)
+        {
+            scriptureText += _words[i].GetDisplayText();
+
+            if (i < _words.Count - 1)
+            {
+                scriptureText += " ";
+            }
+        }
+        return $"{_reference.GetDisplayText()} {scriptureText}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
