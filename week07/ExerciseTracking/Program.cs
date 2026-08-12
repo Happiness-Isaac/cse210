208bcfa03794f0bf130d4d6d0410d6a72ce33fc1
Happiness-Isaac <mmabahappiness@gmail.com>
2026-08-12 11:04:13 +0100

    Week 07

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -7,9 +7,9 @@ class Program
     {
         List<Activity> activities = new List<Activity>();
 
-        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 3.0));
-        activities.Add(new Cycling(new DateTime(2022, 11, 3), 30, 9.7));
-        activities.Add(new Swimming(new DateTime(2022, 11, 3), 30, 20));
+        activities.Add(new Running(new DateTime(2026, 11, 3), 30, 3.0));
+        activities.Add(new Cycling(new DateTime(2026, 11, 3), 30, 9.7));
+        activities.Add(new Swimming(new DateTime(2026, 11, 3), 30, 20));
 
         foreach (Activity activity in activities)
         {
